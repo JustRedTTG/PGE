7f9bf4d5c3a2f05a1f6dbc1a18e170ed288b62c2
RedTTG <46073266+JustRedTTG@users.noreply.github.com>
2021-09-03 17:15:51 +0300

    More colors!

--- colors.py
+++ colors.py
@@ -1,24 +1,30 @@
 """colors"""
+lightred = (200, 0, 0)
 red = (255, 0, 0)
 darkred = (155, 0, 0)
 verydarkred = (55, 0, 0)
 
+lightgreen = (0, 200, 0)
 green = (0, 255, 0)
 darkgreen = (0, 155, 0)
 verydarkgreen = (0, 55, 0)
 
+lightblue = (0, 0, 200)
 blue = (0, 0, 255)
 darkblue = (0, 0, 155)
 verydarkblue = (0, 0, 55)
 
+lightyellow = (200, 200, 0)
 yellow = (255, 255, 0)
 darkyellow = (155, 155, 0)
 verydarkyellow = (55, 55, 0)
 
+lightaqua = (0, 200, 200)
 aqua = (0, 255, 255)
 darkaqua = (0, 155, 155)
 verydarkaqua = (0, 55, 55)
 
+lightpink = (200, 0, 200)
 pink = (255, 0, 255)
 darkpink = (155, 0, 155)
 verydarkpink = (55, 0, 55)
@@ -32,3 +38,9 @@ lightgray = (200, 200, 200)
 gray = (100, 100, 100)
 darkgray = (50, 50, 50)
 verydarkgray = (25, 25, 25)
+
+indigo = (75, 0, 130)
+purple = (128, 0, 128)
+violet = (138, 43, 226)
+
+lavender = (230, 230, 250)
