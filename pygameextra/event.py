96c0a71ad08b6d8800e715cc16025f23385018dc
RedTTG <redtonehair@gmail.com>
2022-07-25 15:44:04 +0300

    optimizations to event script

--- pygameextra/event.py
+++ pygameextra/event.py
@@ -8,7 +8,7 @@ event_buffer = None
 
 def Pquit():
     pygame.quit()
-    quit()
+    exit()
 
 def get():
     event_buffer = pygame.event.get()
@@ -18,39 +18,26 @@ def quitcheck():
     """quitcheck() -> Bool
     Checks if the window was attempted to be closed and returns a bool accordingly
     """
-    tmp = False
-    if c.type == pygame.QUIT:
-        tmp = True
-    return tmp
+    return c.type == pygame.QUIT
 
 def quitcheckauto():
     """quitcheckauto() -> None
     Checks if the window has been closed and automatically quits the program
     """
-    if quitcheck():
-        Pquit()
+    if quitcheck(): Pquit()
         
 def keylog():
     """keylog() -> int
     Returns all the button pressed or released
     """
-    if c.type == pygame.KEYDOWN or c.type == pygame.KEYUP:
-        return c.key
+    if c.type == pygame.KEYDOWN or c.type == pygame.KEYUP: return c.key
 def key_UP(var):
     """key_UP(key) -> Bool
     Check if a button has been released and returns a bool accordingly
     """
-    if c.type == pygame.KEYUP:
-        if c.key == var:
-            return True
-        else:
-            return False
+    if c.type == pygame.KEYUP: return c.key == var
 def key_DOWN(var):
     """key_DOWN(key) -> Bool
     Checks if a key is pressed and returns a bool accordingly
     """
-    if c.type == pygame.KEYDOWN:
-        if c.key == var:
-            return True
-        else:
-            return False
+    if c.type == pygame.KEYDOWN: return c.key == var
