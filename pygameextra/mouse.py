24b9ec4c2da08f4eff99592e7ec2d774adc7830f
RedTTG <redtonehair@gmail.com>
2022-08-29 00:10:33 +0300

    area to draggables

--- pygameextra/mouse.py
+++ pygameextra/mouse.py
@@ -1,6 +1,8 @@
+import time
 import pygame
 # noinspection PyUnresolvedReferences
 from pygame.cursors import arrow, diamond, broken_x, tri_left, tri_right
+from pygameextra.rect import Rect
 from pygameextra import settings
 
 
@@ -31,9 +33,19 @@ def clicked(spoof: bool = True):
 class Draggable:
     start_pos = (0, 0)
     active = False
+    pos = (0, 0)
+    area = (0, 0)
+    rect = None
+    ltic = False
 
-    def __init__(self, position: tuple):
+    def make_rect(self):
+        if self.area:
+            self.rect = Rect(*self.pos, *self.area)
+
+    def __init__(self, position: tuple, area: tuple = None):
         self.pos = position
+        self.area = area
+        self.make_rect()
 
     def calculate(self):
         new_pos = self.pos
@@ -42,13 +54,21 @@ class Draggable:
         return new_pos[0] + difference[0], new_pos[1] + difference[1]
 
     def check(self):
-        if clicked()[0] and not self.active:
+        self.make_rect()
+        if self.rect:
+            mouserect = Rect(*pos(), 1, 1)
+            collide = self.rect.colliderect(mouserect) and not settings.button_lock
+        else:
+            collide = True
+        if (collide and clicked()[0] and not self.ltic) and not self.active:
             self.active = True
+            settings.button_lock = time.time()
             self.start_pos = pos()
-
         elif clicked()[0] and self.active:
+            self.ltic = clicked()[0]
             return True, self.calculate()
         elif not clicked()[0] and self.active:
             self.active = False
             self.pos = self.calculate()
+        self.ltic = clicked()[0]
         return False, self.pos
--- tests.py
+++ tests.py
@@ -1,24 +1,99 @@
+import random
+
 import pygameextra as pe
+from pygameextra import settings
 from pygameextra.debug import FreeMode
 from pygameextra.fpslogger import Logger
 print(pe.__version__)
 pe.init()
 
-pe.display.make((250, 250), "Cool", pe.display.DISPLAY_MODE_RESIZABLE)
-
-sprite = pe.Sprite('image.jpg', (25, 25))
-big_sprite = pe.Sprite('image.jpg', (250, 250))
+pe.display.make((500, 500), "Cool", pe.display.DISPLAY_MODE_RESIZABLE)
 
 log = Logger(size=20)
 pe.settings.debugger = FreeMode()
+settings.button_timeout_time = 0
+settings.button_lock_hold = False
+
+class Cube:
+    def __init__(self, size, location, color=pe.colors.white):
+        self.size = size
+        self.loc = location
+        self.draggable = pe.mouse.Draggable(location, self.size)
+        self.color = color
+        self.fake = None
+        self.click = None
+        self.target = (0, 0)
+
+    def draw(self):
+        pe.draw.rect(self.color, (*self.loc, *self.size))
+
+    def call(self):
+        if self.fake:
+            settings.mouse_position = self.fake
+            settings.mouse_clicked = [True, 0, 0]
+        mov, pos = self.draggable.check()
+        self.loc = pos
+        if self.fake:
+            if pe.math.dist(self.loc, self.target) <= 5:
+                self.fake = None
+                self.click = None
+            else:
+                self.fake = pe.math.lerp(self.fake, self.target, .1)
+        return mov
+
+    def target_m(self, place):
+        self.target = place
+        self.fake = (self.loc[0]+2, self.loc[1]+2)
+
+cubes = []
+x = 0
+y = 0
+ligma = 50
+ranging = ligma ** 2
+
+
+def normalize(start = 0, end = 0, fitmin = 0, fitmax = 255, value = 0):
+    n = (value - start) / (end - start)
+    return fitmin + n * (fitmax - fitmin)
+
+
+for i in range(ligma):
+    for i1 in range(ligma):
+        r = normalize(0, ranging, 100, 255, i*i1)
+        g = normalize(0, ranging, 50, 100, i*i1)
+        b = normalize(0, ranging, 60, 255, i*i1)
+        cubes.append(Cube((10, 10), (x, y), (r, g, b)))
+        y += 10
+    x += 10
+    y = 0
+
 while True:
     for pe.event.c in pe.event.get():
         pe.event.quitcheckauto()
     pe.start_recording()
-    pe.fill.interlace((255, 0, 0), 3)
-    big_sprite.display((-50, -20))
+    pe.fill.full(pe.colors.verydarkgray)
+
+    for cube in cubes:
+        cube.draw()
+    cubes.reverse()
+    log.resetwatch('call')
+    settings.enable_spoof = True
+    for cube in cubes:
+        mov = cube.call()
+        pe.event.buttonLocking()
+        if mov:
+            cubes.remove(cube)
+            cubes.insert(0, cube)
+    settings.enable_spoof = False
+    log.stopwatch('call')
+    cubes.reverse()
+
+    for cube in cubes:
+        if cube.fake: continue
+        cube.target_m((random.randint(0, 500), random.randint(0, 500)))
+
     log.render()
-    pe.display.update(200)
+    pe.display.update()
     pe.stop_recording()
     if pe.mouse.clicked()[2]:
         pe.start_debug()
