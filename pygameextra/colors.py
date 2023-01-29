77c1877095d273f4300992969db2f77046cc9d58
RedTTG <redtonehair@gmail.com>
2023-01-29 03:05:12 +0200

    Sync colors from legacy

--- pygameextra/colors.py
+++ pygameextra/colors.py
@@ -1,25 +1,31 @@
 # RGB
+lightred = (200, 0, 0)
 red = (255, 0, 0)
 darkred = (155, 0, 0)
 verydarkred = (55, 0, 0)
 
+lightgreen = (0, 200, 0)
 green = (0, 255, 0)
 darkgreen = (0, 155, 0)
 verydarkgreen = (0, 55, 0)
 
+lightblue = (0, 0, 200)
 blue = (0, 0, 255)
 darkblue = (0, 0, 155)
 verydarkblue = (0, 0, 55)
 
 # Mixes
+lightyellow = (200, 200, 0)
 yellow = (255, 255, 0)
 darkyellow = (155, 155, 0)
 verydarkyellow = (55, 55, 0)
 
+lightaqua = (0, 200, 200)
 aqua = (0, 255, 255)
 darkaqua = (0, 155, 155)
 verydarkaqua = (0, 55, 55)
 
+lightpink = (200, 0, 200)
 pink = (255, 0, 255)
 darkpink = (155, 0, 155)
 verydarkpink = (55, 0, 55)
@@ -27,6 +33,7 @@ verydarkpink = (55, 0, 55)
 # Monochrome
 black = (0, 0, 0)
 white = (255, 255, 255)
+
 lightgray = (200, 200, 200)
 gray = (100, 100, 100)
 darkgray = (50, 50, 50)
@@ -35,3 +42,9 @@ verydarkgray = (25, 25, 25)
 # Pygame Extra, exclusive colors
 pge_dark = (47, 123, 189)
 pge_light = (150, 206, 255)
+
+# Legacy
+indigo = (75, 0, 130)
+purple = (128, 0, 128)
+violet = (138, 43, 226)
+lavender = (230, 230, 250)
