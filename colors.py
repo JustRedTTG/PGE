506df61e30cde36cbac96c5fd39003832f404368
RedTTG <redtonehair@gmail.com>
2023-01-29 02:31:13 +0200

    Update legacy colors

--- colors.py
+++ colors.py
@@ -1,5 +1,7 @@
 """colors"""
-lightred = (200, 0, 0)
+
+# RGB
+lightred(200, 0, 0)
 red = (255, 0, 0)
 darkred = (155, 0, 0)
 verydarkred = (55, 0, 0)
@@ -14,6 +16,7 @@ blue = (0, 0, 255)
 darkblue = (0, 0, 155)
 verydarkblue = (0, 0, 55)
 
+# Mixes
 lightyellow = (200, 200, 0)
 yellow = (255, 255, 0)
 darkyellow = (155, 155, 0)
@@ -29,18 +32,21 @@ pink = (255, 0, 255)
 darkpink = (155, 0, 155)
 verydarkpink = (55, 0, 55)
 
-
-white = (255, 255, 255)
-
+# Monochrome
 black = (0, 0, 0)
+white = (255, 255, 255)
 
 lightgray = (200, 200, 200)
 gray = (100, 100, 100)
 darkgray = (50, 50, 50)
 verydarkgray = (25, 25, 25)
 
+# Pygame Extra, exclusive colors
+pge_dark = (47, 123, 189)
+pge_light = (150, 206, 255)
+
+# Legacy
 indigo = (75, 0, 130)
 purple = (128, 0, 128)
 violet = (138, 43, 226)
-
 lavender = (230, 230, 250)
