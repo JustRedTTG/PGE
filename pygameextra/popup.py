f4f4a4c3caf0dddaaab8032b18aa02dee9684740
RedTTG <redtonehair@gmail.com>
2023-01-29 02:54:31 +0200

    Fix popup library

--- pygameextra/popup.py
+++ pygameextra/popup.py
@@ -1,12 +1,12 @@
 import pygameextra as pe
-from libraries.roundrect import rect as roundrect
+from pygameextra.roundrect import rect as roundrect
 taken = None
 taken_size = ()
 
-font = pe.pygame.font.SysFont('freesans', 20)
+font = None
 class popup:
     def __init__(self, size: tuple, title: str = "Popup", position: tuple = None):
-        global taken, taken_size
+        global taken, taken_size, font
         taken = pe.display_a
         taken_size = pe.display.get.size()
         self.screen = pe.pygame.Surface(size,pe.pygame.SRCALPHA)
@@ -15,6 +15,7 @@ class popup:
         sizediv = (self.size[0] / 2, self.size[1] / 2)
         takendiv = (taken_size[0] / 2, taken_size[1] / 2)
         pos = (takendiv[0] - sizediv[0] + 20 * int(len(title) / 2.8), takendiv[1] - sizediv[1] + 12)
+        if not font: font = pe.pygame.font.SysFont('freesans', 20)
         if position != None:
             pos = list(position)
             pos[0] += 20 * int(len(title) / 2.8)
@@ -27,6 +28,7 @@ class popup:
         if position != None:
             self.pos = position
         pe.mouse.off = (self.pos[0] * -1, self.pos[1] * -1)
+
 
 def close(popto):
     pop = popto.screen
