a5dfd1e67af68586dd2659d8d3256be96c490332
red <red@redttg.com>
2024-05-26 17:02:40 +0300

    add PGE icon animation

--- assets_raw/compile assets.py
+++ assets_raw/compile assets.py
@@ -2,27 +2,37 @@ import os.path
 
 from pygameextra import Atlas, Sheet, SheetHorizontal
 import pygameextra as pe
+import time
 
 pe.init((0, 0))
 
 SCRIPT_PATH = os.path.realpath(os.path.dirname(__file__))
 DEBUG_DIRECTORY = os.path.join(SCRIPT_PATH, "atlas surfaces")
-COLORS_FOR_LABELING = ["red", "blue",
-                       "yellow", "aqua", "pink"]
+COLORS_FOR_LABELING = ["red", "blue", "pink", "darkaqua"]
 
 os.makedirs(DEBUG_DIRECTORY, exist_ok=True)
+
+print("Compiling atlases...", end='\t')
+start = time.time()
 
 atlases = {
     "loading_icon": Atlas.from_sheets({
         "loading_icon": Sheet(os.path.join(SCRIPT_PATH, "loading_icon.png"), SheetHorizontal(64, 64), 20, loop=True),
         "loading_icon_progress": Sheet(os.path.join(SCRIPT_PATH, "loading_icon_progress.png"), SheetHorizontal(64, 64), 0),
+        "pge_icon": Sheet(os.path.join(SCRIPT_PATH, "pge_icon.png"), SheetHorizontal(96, 48), 20),
     })
 }
+
+print(f"DONE!\t[{time.time()-start:.2f}s]")
 
 for atlas_name, atlas in atlases.items():
     atlas.save(os.path.join(SCRIPT_PATH, '..', 'pygameextra', 'assets', f'{atlas_name}.atlas'))
     atlas.surface.save_to_file(os.path.join(DEBUG_DIRECTORY, f'{atlas_name} (unlabeled).png'))
     color_index = 0
+
+    print(f"Creating atlas labels ({atlas_name}) ...", end='\t')
+    start = time.time()
+
     with atlas.surface:
         pe.fill.transparency(pe.colors.black, 100)
         for mapping in atlas.mappings.values():
@@ -33,11 +43,10 @@ for atlas_name, atlas in atlases.items():
                 rect = pe.Rect(*rect)
                 rect.scale_by_ip(.8, .8)
                 pe.draw.rect(color, rect, 2, edge_rounding=5)
-                pe.text.quick(str(i), rect.width//3, rect.center).display()
+                pe.Text(str(i), font_size=rect.width//3, position=rect.center, colors=(pe.colors.white, pe.colors.black)).display()
 
         color_index = 0
         for mapping_name, mapping in atlas.mappings.items():
-            print(mapping_name)
             color = getattr(pe.colors, COLORS_FOR_LABELING[color_index])
             color_index += 1
             rect = mapping[0]
@@ -45,7 +54,8 @@ for atlas_name, atlas in atlases.items():
             text.rect.topleft = rect[:2]
             text.rect.top += 1
             text.rect.left += 1
+            text.rect.right = min(pe.display.get_width(), text.rect.right)
             text.display()
-
+    print(f"DONE!\t[{time.time()-start:.2f}s]")
 
     atlas.surface.save_to_file(os.path.join(DEBUG_DIRECTORY, f'{atlas_name} (labeled).png'))
--- pygameextra/assets/__init__.py
+++ pygameextra/assets/__init__.py
@@ -3,3 +3,4 @@ import os
 __asset_location = os.path.realpath(os.path.dirname(__file__))
 
 ASSET_FONT = os.path.join(__asset_location, 'font.ttf')
+ASSET_LOADING_ICON = os.path.join(__asset_location, 'loading_icon.atlas')
